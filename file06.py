4da741356e07fbea69104e4b6c6e77bc0f94676f
Roma1125 <ramazonomonturdiyev17@gmail.com>
2022-11-02 15:08:46 +0500

    file06.py

--- file06.py
+++ file06.py
@@ -6,5 +6,16 @@ def main(data:str):
     Returns:
         list: return answer
     """
+    l=data.split('\n')
+    f=[]
+    for i in range(len(l)):
+        f.append(len(l[i]))
+
+   
     
+    return f
+f=open('txt_file/data06.txt')
+d=f.read()
+print((main(d)))
+# Read data from file
 # Read data from file
