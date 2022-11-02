eb702bab604e2f2f541da1b9c06766d22e34fb9a
Roma1125 <ramazonomonturdiyev17@gmail.com>
2022-11-02 13:46:31 +0500

    file05.py

--- file05.py
+++ file05.py
@@ -6,5 +6,25 @@ def main(data:str):
     Returns:
         list: return answer
     """
+    l=[]
+    f=0
+    d=0
+    for i in range(len(data)):
+        
+        if not data[i].isdigit():
+            f+=1
+            
+        if  data[i].isdigit():
+            d+=1
+        
+        
     
+
+    
+    l.append(d)    
+    l.append(f)
+    return l
+f=open('txt_file/data05.txt')
+d=f.read()
+print((main(d)))
 # Read data from file
