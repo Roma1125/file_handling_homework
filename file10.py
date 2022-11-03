62632d0bf730909f61be30b8f65d4b9e7f48b5ce
Roma1125 <ramazonomonturdiyev17@gmail.com>
2022-11-03 14:26:40 +0500

    dfdsfs

--- file10.py
+++ file10.py
@@ -6,5 +6,23 @@ def main(data:str):
     Returns:
         int: return answer
     """
+    f=data.split('\n')
+    l=0
+    for i in f:
+        
+        if  l<len(i):
+             l=int(i)
+        
+        
+        
+        
 
+          
+
+    return l
+f=open('txt_file/data10.txt')
+d=f.read()
+
+print((main(d)))
+# Read data from file
 # Read data from file
