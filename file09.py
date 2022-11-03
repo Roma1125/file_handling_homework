16fd6af0eb3c2e74f5183316a150a6a9ef578986
Roma1125 <ramazonomonturdiyev17@gmail.com>
2022-11-03 14:18:36 +0500

    fdd

--- file09.py
+++ file09.py
@@ -7,4 +7,21 @@ def main(data:str):
         int: return answer
     """
 
+    l=9
+    for i in data:
+        
+        if i.isdigit() and l>int(i):
+             l=int(i)
+        
+        
+        
+    
+
+          
+
+    return l
+f=open('txt_file/data09.txt')
+d=f.read()
+
+print((main(d)))
 # Read data from file
